6286be1695d092b3b915a38d20ba210883f7da21
Alex Forsythe <awforsythe@gmail.com>
2020-01-24 16:24:18 -0600

    Update FramesContext to update in real time

--- frontend/src/contexts/FramesContext.jsx
+++ frontend/src/contexts/FramesContext.jsx
@@ -6,17 +6,35 @@ import { expectJson } from '../util.jsx';
 const FramesContext = React.createContext();
 
 class FramesProvider extends React.Component {
-  constructor() {
+  constructor(props) {
     super(props);
     this.state = {
       isLoading: false,
       error: null,
       frames: [],
     };
+    this.socket = null;
   }
+
+  handleEvent = (event, params) => {
+    if (event === 'frame_created') {
+      if (params.sessionId === this.props.sessionId) {
+        this.fetchSingleFrame(params.id);
+      }
+    }
+  };
 
   componentDidMount() {
     this.fetchFrames();
+    this.socket = new WebSocket(`ws://${window.location.host}/ws`);
+    this.socket.onmessage = (sockevent) => {
+      const { event, params } = JSON.parse(sockevent.data);
+      this.handleEvent(event, params);
+    }
+  }
+
+  componentWillUnmount() {
+    this.socket.close(1001);
   }
 
   fetchFrames() {
@@ -25,6 +43,24 @@ class FramesProvider extends React.Component {
       .then(expectJson)
       .then(data => this.setState({ isLoading: false, error: null, frames: data.frames }))
       .catch(error => this.setState({ isLoading: false, error }));
+  }
+
+  fetchSingleFrame(id) {
+    if (!this.state.isLoading) {
+      fetch(`/api/frame/${id}`)
+        .then(expectJson)
+        .then(data => {
+          const { isLoading, frames } = this.state;
+          if (!isLoading) {
+            const index = frames.findIndex(x => x.number > data.number);
+            if (index >= 0) {
+              this.setState({ frame: frames.slice(0, index).concat([data]).concat(frames.slice(index)) });
+            } else {
+              this.setState({ frames: frames.concat([data]) })
+            }
+          }
+        });
+    }
   }
 
   render() {
